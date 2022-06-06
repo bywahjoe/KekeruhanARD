50dcaa91abb424e4bcee062de63a1d4e9ec44110
bywahjoe <wahjoe.why@gmail.com>
2022-06-06 13:21:52 +0700

    V1.1

--- kekeruhan/kekeruhan.ino
+++ kekeruhan/kekeruhan.ino
@@ -20,13 +20,18 @@ void setup() {
 
 void loop() {
   //LCD Run
+  int valSensor = getKekeruhan();
   lcd.setCursor(0, 0);
-  lcd.print("V. Sensor: ");
-  lcd.print(getKekeruhan());
+  lcd.print("Keruh: ");
+  lcd.print(valSensor);
+  lcd.print(" NTU");
 
+  int analogVal=analogRead(sensor_kekeruhan);
   lcd.setCursor(0, 1);
-  lcd.print("V. Analog: ");
-  lcd.print(analogRead(sensor_kekeruhan));
+  lcd.print("V: ");
+  lcd.print(analogVal);
+  lcd.print(cekKeruh(analogVal));
+
   delay(2000);
   lcd.clear();
 }
@@ -47,4 +52,16 @@ int getKekeruhan() {
   if (volt < 2.5) ntu = 3000;
   else ntu = -1120.4 * square(volt) + 5742.3 * volt - 4353.8;
   return ntu;
+}
+
+String cekKeruh(int val) {
+  if (val > 870) {
+    return "/JERNIH";
+  }
+  else if(val > 500) {
+    return "/SEDANG";
+  }
+  else {
+    return "/KERUH";
+  }
 }
